3d4826305d2a0e983c03169df0101c2349837904
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-21 11:33:21 +0700

    Implement PageUsers with axios

--- src/PageUsers/index.js
+++ src/PageUsers/index.js
@@ -1,29 +1,51 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
-const NAME = process.env.REACT_APP_NAME
 const API_URL = process.env.REACT_APP_API_URL
 
 const request = axios.create({
   baseURL: API_URL,
-  timeout: 1000
+  timeout: 5000
 })
 
 class PageUsers extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
-      users: []
+      users: [],
+      error: null
     }
+
+    console.log(API_URL)
+
+    request
+      .get('/users')
+      .then(response => {
+        this.setState({ users: response.data.users })
+      })
+      .catch(error => {
+        console.log(error)
+        this.setState({ error })
+      })
   }
 
   render() {
+    const users = this.state.users
+
     return (
       <div>
+        <h3>Users</h3>
         <ul>
-          {this.state.users.map(user => {
-            return <li key={user.id}>{user.name}</li>
-          })}
+          {users &&
+            users.map(user => {
+              return (
+                <li key={user.id}>
+                  <Link to={`/profile/${user.id}`}>{user.username}</Link>
+                </li>
+              )
+            })}
         </ul>
       </div>
     )
